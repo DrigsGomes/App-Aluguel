5be266ede18bbb664e1f93a54e449b5512f2d5dc
DrigsGomes <rodrigo.vitor169@gmail.com>
2021-04-10 21:46:40 -0300

    Criando a estrutura do App

--- AppAluguel/App.js
+++ AppAluguel/App.js
@@ -1,7 +1,25 @@
+import 'react-native-gesture-handler';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
 import { View, Text, StyleSheet} from 'react-native';
+import {AppLoading} from 'expo';
+import {useFonts, Montserrat_400Regular, Montserrat_500Medium, 
+  Montserrat_700Bold} from '@expo-google-fonts/montserrat';
+
 
 export default function App() {
+
+  let [fontsLoaded] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_500Medium,
+    Montserrat_700Bold
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading/>;
+  }
+
+
  return (
    <View style={styles.container}>
      <Text style={styles.title} > Test </Text>
