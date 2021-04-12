4b8aba6b99bc916cbac4dc314f802a7158c70bcc
DrigsGomes <rodrigo.vitor169@gmail.com>
2021-04-11 21:16:56 -0300

    Criando as rotas

--- AppAluguel/App.js
+++ AppAluguel/App.js
@@ -6,6 +6,9 @@ import {AppLoading} from 'expo';
 import {useFonts, Montserrat_400Regular, Montserrat_500Medium, 
   Montserrat_700Bold} from '@expo-google-fonts/montserrat';
 
+import Routes from './src/router';
+
+
 
 export default function App() {
 
@@ -21,8 +24,10 @@ export default function App() {
 
 
  return (
-   <View >
-     <Text> Test </Text>
-   </View>
+   <>
+   <StatusBar style="light" backgroundColor="#000" translucent={false}/>
+   <Routes></Routes>
+
+   </>
   );
 }; 
---
+++ AppAluguel/src/router.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {Feather} from '@expo/vector-icons';
+
+import Home from './pages/Home';
+import Detail from './pages/Datail';
+
+
+const Stack = createStackNavigator();
+
+function Routes (){
+    return(
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="home" component={Home}/>
+                <Stack.Screen name="detail" component={Detail}/>
+
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export  default Routes;
