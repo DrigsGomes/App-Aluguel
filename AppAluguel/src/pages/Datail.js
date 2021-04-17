7d45e2df0fbd9a6931834669bbc307a9c05fe9cc
DrigsGomes <rodrigo.vitor169@gmail.com>
2021-04-16 22:25:25 -0300

    Update Datail.js

--- AppAluguel/src/pages/Datail.js
+++ AppAluguel/src/pages/Datail.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import { View, Text} from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import {Ionicons, Feather} from '@expo/vector-icons';
+import {ScrollView} from 'react-native-gesture-handler';
 
 export default function Detail() {
  return (
-   <View>
-       <Text>DETAIL</Text>
+   <View style={styles.container}>
+
+     <View style={styles.content}>
+
+     </View>
    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:"#FFF",
+  },
+
+  content:{
+    flexDirection:'row',
+    height:340,
+    height:'100%',
+  },
+
+
+
+});
